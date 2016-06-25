3270bfc07e7c5f7aed04bcfc060e8dd3940f68b7
Solace7 <elijahjuanye@gmail.com>
2016-06-24 18:19:53 -0700

    fix case in function, add debug code

--- Redstone Movement API/movementAPI.lua
+++ Redstone Movement API/movementAPI.lua
@@ -28,11 +28,11 @@ move.state = {
 
 local rim = {}
 
---new function for movement State. May move to separate file
+--new function for movement state. May move to separate file
 function move.state.toString(state)
-  if state == move.State.MOVING then print("MOVING")
-  elseif state == move.State.STATIONARY then print("STATIONARY")
-  elseif state == move.State.ERROR then print ("ERROR")
+  if state == move.state.MOVING then print("MOVING")
+  elseif state == move.state.STATIONARY then print("STATIONARY")
+  elseif state == move.state.ERROR then print ("ERROR")
   else print("UNKNOWN")
 end
 end
@@ -40,48 +40,48 @@ end
 function rim.move(direction,duration)
   if direction == west then
     trigger(sides.bottom,colors.yellow,255)
-    ----move.State = "MOVING"
-    ----print(move.State .. direction)
+    move.state = MOVING
+    print(move.state .. direction)
     os.sleep(duration)
     trigger(sides.bottom,colors.yellow,0)
 
   elseif direction == east then
   	trigger(sides.bottom,colors.magenta,255)
-    --move.State = "MOVING"
-    ----print(move.State .. direction)
+    move.state = MOVING
+    print(move.state .. direction)
   	os.sleep(duration)
   	trigger(sides.bottom,colors.magenta,0)
 
   elseif direction == south then
   	trigger(sides.bottom,colors.lime,255)
-    --move.State = "MOVING"
-    --print(move.State .. direction)
+    move.state = MOVING
+    print(move.state .. direction)
   	os.sleep(duration)
   	trigger(sides.bottom,colors.lime,0)
 
   elseif direction == north then
   	trigger(sides.bottom,colors.blue,255)
-    --move.State = "MOVING"
-    --print(move.State .. direction)
+    move.state = MOVING
+    print(move.state .. direction)
   	os.sleep(duration)
   	trigger(sides.bottom,colors.blue,0)
 
   elseif direction == up then
   	trigger(sides.bottom,colors.white,255)
-    --move.State = "MOVING"
-    --print(move.State .. direction)
+    move.state = MOVING
+    print(move.state .. direction)
   	os.sleep(duration)
   	trigger(sides.bottom,colors.white,0)
 
   elseif direction == down then
   	trigger(sides.bottom,colors.orange,255)
-    --move.State = "MOVING"
-    --print(move.State .. direction)
+    move.state = MOVING
+    print(move.state .. direction)
   	os.sleep(duration)
   	trigger(sides.bottom,colors.orange,0)
-  	
+
 	else
-	  print("Input a direction")
+	  print("Input a direction, and duration")
 	end
 end
 
